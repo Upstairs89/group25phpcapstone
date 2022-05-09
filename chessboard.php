071fa2fd46a315b4ff2aa59d13ec238a8eb33eef
Victor Okeyinka <brownvktr@gmail.com>
2022-05-09 17:54:23 +0100

    Update chessboard.php

--- chessboard.php
+++ chessboard.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,20 +8,26 @@
     <title>Chessboard</title>
 </head>
 <body>
-    <table width="300px" cellspacing="0px" cellpadding="0px" border="1px">
-        <?php 
-        for($row = 1; $row <= 8; $row++ ){
+<table width="300px" cellspacing="0px" cellpadding="0px" border="1px">
+    <?php
+        for($row = 1; $row <= 8; $row++ ) // loop from 1 to 8
+        {   
             echo "<tr>";
-            for($col = 1; $col <= 8; $col++ ){
-                $field = $row + $col;
-                if ($field % 2 == 0){
-                    echo "<td height=30px width=30px bgcolor=white></td>";
-                }else{
-                    echo "<td height=30px width=30px bgcolor=black></td>";
-                }
+            for($col = 1; $col <= 8; $col++ ) // loop from 1 to 8
+            {
+               $field = $row + $col; // add row and col and then iterate
+
+            if ($field % 2 == 0) // evaluate where field is even. (i.e 1+1 = 2/2 = 0)
+             {
+             echo "<td height=30px width=30px bgcolor=white></td>";
+             }
+            else
+            {
+             echo "<td height=30px width=30px bgcolor=black></td>";
             }
+         }
         }
-        ?>
-    </table>
+    ?>
+</table>
 </body>
 </html>
